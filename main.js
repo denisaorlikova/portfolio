8d341697e211d0ae17e7a302d019518eb1be667b
Denisa <denisa@denisaorlikova.com>
2020-07-29 11:38:19 +0200

    change color palette to pastels

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
 $(document).ready(function(){
   $(".my-overlay-blue").hover(function(){
-  $(this).css("background-color", "hsla(0, 0%, 100%, 0.4)");
+  $(this).css("background-color", "hsla(0, 0%, 100%, 0.3)");
   $(this).find("a").css("display","inline").find("h5").css("display","none");
   $(this).find("h5").css("display","none");
   }, function(){
-  $(this).css("background-color", "hsla(218, 89%, 57%, 0.8)").find("a").css("display","none").find("h5").css("display","inline");
+  $(this).css("background-color", "hsla(210, 100%, 80%, 0.9)").find("a").css("display","none").find("h5").css("display","inline");
   $(this).find("a").css("display","none");
   $(this).find("h5").css("display","inline");
 });
 });
 
 $(document).ready(function(){
-  $(".my-overlay-orange").hover(function(){
-  $(this).css("background-color", "hsla(0, 0%, 100%, 0.4)");
+  $(".my-overlay-purple").hover(function(){
+  $(this).css("background-color", "hsla(0, 0%, 100%, 0.3)");
   $(this).find("a").css("display","inline").find("h5").css("display","none");
   $(this).find("h5").css("display","none");
   }, function(){
-  $(this).css("background-color", "hsla(17, 89%, 57%, 0.8)").find("a").css("display","none").find("h5").css("display","inline");
+  $(this).css("background-color", "hsla(270, 100%, 80%, 0.9)").find("a").css("display","none").find("h5").css("display","inline");
   $(this).find("a").css("display","none");
   $(this).find("h5").css("display","inline");
 });
 });
 
 $(document).ready(function(){
-  $(".my-overlay-yellow").hover(function(){
-  $(this).css("background-color", "hsla(0, 0%, 100%, 0.4)");
+  $(".my-overlay-mint").hover(function(){
+  $(this).css("background-color", "hsla(0, 0%, 100%, 0.3)");
   $(this).find("a").css("display","inline").find("h5").css("display","none");
   $(this).find("h5").css("display","none");
   }, function(){
-  $(this).css("background-color", "hsl(46, 89%, 57%, 0.8)").find("a").css("display","none").find("h5").css("display","inline");
+  $(this).css("background-color", "hsla(150, 100%, 80%, 0.9)").find("a").css("display","none").find("h5").css("display","inline");
   $(this).find("a").css("display","none");
   $(this).find("h5").css("display","inline");
 });
